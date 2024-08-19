0dc406b62174b4c7f12f717d7405e5a8537b36cf
andreaDevelope <andrea_develope@outlook.it>
2024-08-19 15:28:32 +0200

    finish

--- D1.js
+++ D1.js
@@ -12,12 +12,41 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+// Tipo di dato: Numero
+const numero = 42;
+
+// Tipo di dato: Stringa
+const stringa = "Ciao mondo";
+
+// Tipo di dato: Booleano
+let booleano = true;
+
+// Tipo di dato: Oggetto
+const oggetto = { nome: "Mario", età: 30 };
+
+// Tipo di dato: Array
+const array = [1, 2, 3, 4];
+
+// Tipo di dato: Funzione
+const funzione = function () {
+  return "Hello!";
+};
+
+// Tipo di dato: Undefined (variabile dichiarata ma non definita)
+let indefinito;
+
+// Tipo di dato: Null (assenza intenzionale di valore)
+const nullo = null;
+
+// Tipo di dato: Symbol (crea un identificatore univoco)
+const simbolo = Symbol("id");
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const myName = "andrea";
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
@@ -25,11 +54,20 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+console.log(12 + 20);
+
+const num1 = 12;
+const num2 = 20;
+const result = num1 + num2;
+console.log("stampo in console la costante result " + result);
+
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+const x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
@@ -38,11 +76,16 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+// dimostro che non si può fare
+
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+const result2 = 4 - x;
+console.log("stampo la variable result2 dell' esercizio 6 " + result2);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -51,3 +94,15 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+const name1 = "john";
+const name2 = "John";
+
+name1 === name2 ? console.log("i nomi sono uguali") : console.log("i nomi sono diversi");
+
+// lo rifaccio con l' operatore logico not
+
+const name3 = "john";
+const name4 = "John";
+
+name3 !== name4 ? console.log("i nomi sono diversi") : console.log("i nomi sono uguali");
